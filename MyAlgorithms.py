16daeed6d98d26778fd6bd8fe2b7b86c86ca40f9
Сергей <sergeyruk312@gmail.com>
2022-11-08 20:17:34 +0300

    Now the "comp_func" is optional

--- MyAlgorithms.py
+++ MyAlgorithms.py
@@ -1,10 +1,15 @@
-def insertion_sort(arr, comp_func):
+def insertion_sort(arr, comp_func=None):
     for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
-        while i >= 0 and comp_func(arr[i], key):
-            arr[i + 1] = arr[i]
-            i = i - 1
+        if comp_func is None:
+            while i >= 0 and arr[i] > key:
+                arr[i + 1] = arr[i]
+                i = i - 1
+        else:
+            while i >= 0 and comp_func(arr[i], key):
+                arr[i + 1] = arr[i]
+                i = i - 1
         arr[i + 1] = key
     return arr
 
