1e4e4a0734689a466b6ec5c6a868a61584eeeb05
Сергей <sergeyruk312@gmail.com>
2022-11-15 14:28:32 +0300

    Changed a merge and merge_sort functions

--- MyAlgorithms.py
+++ MyAlgorithms.py
@@ -33,43 +33,56 @@ def comp(a, b):
     pass
 
 
-def merge(A, p, q, r):
-    n_1 = q - p + 1
-    n_2 = r - q
-    left = []
-    right = []
-    for i in range(0, n_1):
-        left.append(A[p + i])
-    for j in range(0, n_2):
-        right.append(A[q + j + 1])
-    i = 0
-    j = 0
-    min_n = n_1 if n_1 <= n_2 else n_2
-    for k in range(p, p + min_n):
-        if left[i] <= right[j]:
-            A[k] = left[i]
-            i += 1
+def merge(arr1, arr2, destination=None, comp_func=None):
+    if destination is None:
+        destination = []
+    i = j = 0
+    len1 = len(arr1)
+    len2 = len(arr2)
+    if comp_func is None:
+        while i < len1 and j < len2:
+            if arr1[i] < arr2[j]:
+                destination.append(arr1[i])
+                i += 1
+            else:
+                destination.append(arr2[j])
+                j += 1
+        if i == len1:
+            while j < len2:
+                destination.append(arr2[j])
+                j += 1
         else:
-            A[k] = right[j]
-            j += 1
-    for k in range(p + min_n, r + 1):
-        if n_1 >= n_2:
-            A[k] = left[i]
-            i += 1
+            while i < len1:
+                destination.append(arr1[i])
+                i += 1
+    else:
+        while i < len1 and j < len2:
+            if comp_func(arr2[i], arr2[j]):
+                destination.append(arr1[i])
+                i += 1
+            else:
+                destination.append(arr2[j])
+                j += 1
+        if i == len1:
+            while j < len2:
+                destination.append(arr2[j])
+                j += 1
         else:
-            A[k] = right[j]
-            j += 1
-    pass
+            while i < len1:
+                destination.append(arr1[i])
+                i += 1
+    return destination
 
 
-def merge_sort(A, p, r):
-    # make p and r optional and add optional compare function
-    if p < r:
-        q = math.floor((p + r) / 2)
-        merge_sort(A, p, q)
-        merge_sort(A, q + 1, r)
-        merge(A, p, q, r)
-    pass
+def merge_sort(array, comp_func=None, bot=0, top=None):
+    if top is None:
+        top = len(array)
+    if bot < top:
+        mid = math.floor((bot + top) / 2)
+        merge_sort(array, bot=bot, top=mid)
+        merge_sort(array, bot=mid + 1, top=top)
+        print(array[bot:mid], array[mid + 1: top])
+        merge(array[bot:mid], array[mid + 1: top], comp_func=comp_func)
 
 
 def binary_search(arr, des_el):
@@ -77,15 +90,20 @@ def binary_search(arr, des_el):
     top = len(arr) - 1
     if arr[bot] > des_el or arr[top] < des_el:
         return None
-    mid = math.floor((bot + top)/2)
+    mid = math.floor((bot + top) / 2)
     while arr[mid] != des_el and bot != top:
         if mid >= des_el:
             top = mid
         else:
             bot = mid
-        mid = math.floor((bot + top)/2)
+        mid = math.floor((bot + top) / 2)
     if arr[mid] == des_el:
         return mid
     else:
         return None
     pass
+
+
+arr = [5, 2, 3, 1, 10, 4, 7]
+#merge_sort(arr)
+print(arr[0:4])
